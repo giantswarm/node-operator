f36a575e6a4e42ccaa5344122b084cb9597d804d
Tim Schindler <xh3b4sd@gmail.com>
2018-01-29 12:20:57 +0100

    added basic project structure

---
+++ service/service.go
@@ -0,0 +1,116 @@
+package service
+
+import (
+	"sync"
+
+	"github.com/giantswarm/microendpoint/service/version"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/giantswarm/operatorkit/client/k8sclient"
+	"github.com/spf13/viper"
+	"k8s.io/client-go/kubernetes"
+
+	"github.com/giantswarm/node-operator/flag"
+	"github.com/giantswarm/node-operator/service/healthz"
+)
+
+type Config struct {
+	Logger micrologger.Logger
+
+	Description string
+	Flag        *flag.Flag
+	GitCommit   string
+	Name        string
+	Source      string
+	Viper       *viper.Viper
+}
+
+func DefaultConfig() Config {
+	return Config{
+		Logger: nil,
+
+		Description: "",
+		Flag:        nil,
+		GitCommit:   "",
+		Name:        "",
+		Source:      "",
+		Viper:       nil,
+	}
+}
+
+type Service struct {
+	Healthz *healthz.Service
+	Version *version.Service
+
+	bootOnce sync.Once
+}
+
+func New(config Config) (*Service, error) {
+	if config.Flag == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.Flag must not be empty")
+	}
+	if config.Viper == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.Viper must not be empty")
+	}
+
+	var err error
+
+	var k8sClient kubernetes.Interface
+	{
+		k8sConfig := k8sclient.DefaultConfig()
+
+		k8sConfig.Address = config.Viper.GetString(config.Flag.Service.Kubernetes.Address)
+		k8sConfig.Logger = config.Logger
+		k8sConfig.InCluster = config.Viper.GetBool(config.Flag.Service.Kubernetes.InCluster)
+		k8sConfig.TLS.CAFile = config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.CAFile)
+		k8sConfig.TLS.CrtFile = config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.CrtFile)
+		k8sConfig.TLS.KeyFile = config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.KeyFile)
+
+		k8sClient, err = k8sclient.New(k8sConfig)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var healthzService *healthz.Service
+	{
+		healthzConfig := healthz.DefaultConfig()
+
+		healthzConfig.K8sClient = k8sClient
+		healthzConfig.Logger = config.Logger
+
+		healthzService, err = healthz.New(healthzConfig)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var versionService *version.Service
+	{
+		versionConfig := version.DefaultConfig()
+
+		versionConfig.Description = config.Description
+		versionConfig.GitCommit = config.GitCommit
+		versionConfig.Name = config.Name
+		versionConfig.Source = config.Source
+
+		versionService, err = version.New(versionConfig)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	newService := &Service{
+		Healthz: healthzService,
+		Version: versionService,
+
+		bootOnce: sync.Once{},
+	}
+
+	return newService, nil
+}
+
+func (s *Service) Boot() {
+	s.bootOnce.Do(func() {
+	})
+}
