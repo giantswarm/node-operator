c10964ed9c2bbc76ed500152ef575f0b1c4c767f
Tim Schindler <xh3b4sd@gmail.com>
2018-02-01 17:17:04 +0100

    added comments to have a better overview of the next steps

--- service/nodeconfig/v1/resource/node/current.go
+++ service/nodeconfig/v1/resource/node/current.go
@@ -4,6 +4,9 @@ import (
 	"context"
 )
 
+// GetCurrentState is a noop. Draining nodes does not require more complex CRUD
+// management of resources. See ApplyCreateChange for the business logic
+// implementation.
 func (r *Resource) GetCurrentState(ctx context.Context, obj interface{}) (interface{}, error) {
 	return nil, nil
 }
--- service/nodeconfig/v1/resource/node/update.go
+++ service/nodeconfig/v1/resource/node/update.go
@@ -6,15 +6,15 @@ import (
 	"github.com/giantswarm/operatorkit/framework"
 )
 
+// ApplyUpdateChange is a noop. Draining nodes does not require more complex
+// CRUD management of resources. See ApplyCreateChange for the business logic
+// implementation.
 func (r *Resource) ApplyUpdateChange(ctx context.Context, obj, updateChange interface{}) error {
 	return nil
 }
 
 func (r *Resource) NewUpdatePatch(ctx context.Context, obj, currentState, desiredState interface{}) (*framework.Patch, error) {
 	patch := framework.NewPatch()
+	patch.SetCreateChange(true) // hack to execute ApplyCreateChange
 	return patch, nil
-}
-
-func (r *Resource) newUpdateChange(ctx context.Context, obj, currentState, desiredState interface{}) (interface{}, error) {
-	return nil, nil
 }
