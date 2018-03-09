a6162b9f901e0fdd78e8de6799d32de21fcbf834
Tim Schindler <xh3b4sd@gmail.com>
2018-03-09 16:54:45 +0100

    moved version bundle to versioned package (#25)

---
+++ service/nodeconfig/v1/version_bundle.go
@@ -0,0 +1,36 @@
+package v1
+
+import (
+	"time"
+
+	"github.com/giantswarm/versionbundle"
+)
+
+func VersionBundle() versionbundle.Bundle {
+	return versionbundle.Bundle{
+		Changelogs: []versionbundle.Changelog{
+			{
+				Component:   "node-operator",
+				Description: "Introduce the first version of the node-operator.",
+				Kind:        versionbundle.KindAdded,
+			},
+		},
+		Components: []versionbundle.Component{
+			{
+				Name:    "node-operator",
+				Version: "0.1.0",
+			},
+		},
+		Dependencies: []versionbundle.Dependency{
+			{
+				Name:    "kubernetes",
+				Version: ">= 1.8.4",
+			},
+		},
+		Deprecated: false,
+		Name:       "node-operator",
+		Time:       time.Date(2018, time.December, 6, 18, 6, 0, 0, time.UTC),
+		Version:    "0.1.0",
+		WIP:        true,
+	}
+}
--- service/version_bundles.go
+++ service/version_bundles.go
@@ -1,38 +1,15 @@
 package service
 
 import (
-	"time"
-
 	"github.com/giantswarm/versionbundle"
+
+	"github.com/giantswarm/node-operator/service/nodeconfig/v1"
 )
 
 func NewVersionBundles() []versionbundle.Bundle {
-	return []versionbundle.Bundle{
-		{
-			Changelogs: []versionbundle.Changelog{
-				{
-					Component:   "node-operator",
-					Description: "Introduce the first version of the node-operator.",
-					Kind:        versionbundle.KindAdded,
-				},
-			},
-			Components: []versionbundle.Component{
-				{
-					Name:    "node-operator",
-					Version: "0.1.0",
-				},
-			},
-			Dependencies: []versionbundle.Dependency{
-				{
-					Name:    "kubernetes",
-					Version: ">= 1.8.4",
-				},
-			},
-			Deprecated: false,
-			Name:       "node-operator",
-			Time:       time.Date(2018, time.December, 6, 18, 6, 0, 0, time.UTC),
-			Version:    "0.1.0",
-			WIP:        true,
-		},
-	}
+	var versionBundles []versionbundle.Bundle
+
+	versionBundles = append(versionBundles, v1.VersionBundle())
+
+	return versionBundles
 }
