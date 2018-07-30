4e15ef28bc2cb27ac75b05263495ab0ad46e7a28
Tim Schindler <xh3b4sd@gmail.com>
2018-07-30 17:53:02 +0200

    delete nodes from guest API when drainer config gets deleted

--- service/controller/v1/resource/drainer/delete.go
+++ service/controller/v1/resource/drainer/delete.go
@@ -2,10 +2,54 @@ package drainer
 
 import (
 	"context"
+
+	"github.com/giantswarm/errors/guest"
+	"github.com/giantswarm/microerror"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+
+	"github.com/giantswarm/node-operator/service/controller/v1/key"
 )
 
-// EnsureDeleted is a noop, because the node resource implementation is not
-// interested in delete events of the nodeconfigs.
 func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
+	drainerConfig, err := key.ToDrainerConfig(obj)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	var k8sClient kubernetes.Interface
+	{
+		i := key.ClusterIDFromDrainerConfig(drainerConfig)
+		e := key.ClusterEndpointFromDrainerConfig(drainerConfig)
+		k8sClient, err = r.guestCluster.NewK8sClient(ctx, i, e)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	{
+		r.logger.LogCtx(ctx, "level", "debug", "message", "deleting guest cluster node from Kubernetes API")
+
+		n := key.NodeNameFromDrainerConfig(drainerConfig)
+		o := &metav1.DeleteOptions{}
+		err := k8sClient.CoreV1().Nodes().Delete(n, o)
+		if guest.IsAPINotAvailable(err) {
+			r.logger.LogCtx(ctx, "level", "debug", "message", "guest cluster API is not available")
+			r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
+
+			return nil
+		} else if apierrors.IsNotFound(err) {
+			r.logger.LogCtx(ctx, "level", "debug", "message", "guest cluster node not found")
+			r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
+
+			return nil
+		} else if err != nil {
+			return microerror.Mask(err)
+		}
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", "deleted guest cluster node from Kubernetes API")
+	}
+
 	return nil
 }
