ca7fc4ad2953608540b5fb63bf98aba53e787753
calvix <rozsypalek.vaclav@gmail.com>
2019-01-14 12:32:49 +0100

    v2: copy v1 (#78)

---
+++ service/controller/v2/resource/drainer/delete.go
@@ -0,0 +1,63 @@
+package drainer
+
+import (
+	"context"
+
+	"github.com/giantswarm/errors/guest"
+	"github.com/giantswarm/guestcluster"
+	"github.com/giantswarm/microerror"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+
+	"github.com/giantswarm/node-operator/service/controller/v1/key"
+)
+
+func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
+	drainerConfig, err := key.ToDrainerConfig(obj)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	var k8sClient kubernetes.Interface
+	{
+		i := key.ClusterIDFromDrainerConfig(drainerConfig)
+		e := key.ClusterEndpointFromDrainerConfig(drainerConfig)
+		k8sClient, err = r.guestCluster.NewK8sClient(ctx, i, e)
+		if guestcluster.IsTimeout(err) {
+			r.logger.LogCtx(ctx, "level", "debug", "message", "fetching certificates timed out")
+			r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
+
+			return nil
+		} else if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	{
+		r.logger.LogCtx(ctx, "level", "debug", "message", "deleting guest cluster node from Kubernetes API")
+
+		n := key.NodeNameFromDrainerConfig(drainerConfig)
+		o := &metav1.DeleteOptions{}
+		err := k8sClient.CoreV1().Nodes().Delete(n, o)
+		if guest.IsAPINotAvailable(err) {
+			r.logger.LogCtx(ctx, "level", "debug", "message", "did not delete guest cluster node from Kubernetes API")
+			r.logger.LogCtx(ctx, "level", "debug", "message", "guest cluster API is not available")
+			r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
+
+			return nil
+		} else if apierrors.IsNotFound(err) {
+			r.logger.LogCtx(ctx, "level", "debug", "message", "did not delete guest cluster node from Kubernetes API")
+			r.logger.LogCtx(ctx, "level", "debug", "message", "guest cluster node not found")
+			r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
+
+			return nil
+		} else if err != nil {
+			return microerror.Mask(err)
+		}
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", "deleted guest cluster node from Kubernetes API")
+	}
+
+	return nil
+}
