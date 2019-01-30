5b4fa2819d56b900dfa911cc6c05fb70d4ff9106
Tim Schindler <xh3b4sd@gmail.com>
2019-01-30 12:12:38 +0100

    drop node config logic (#84)

--- service/controller/v2/resource/node/delete.go
+++ service/controller/v2/resource/node/delete.go
@@ -4,8 +4,6 @@ import (
 	"context"
 )
 
-// EnsureDeleted is a noop, because the node resource implementation is not
-// interested in delete events of the nodeconfigs.
 func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
 	return nil
 }
