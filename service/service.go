010df24b717618020897ab2dc20f21ca94f2f92f
Ross Fairbanks <ross@giantswarm.io>
2019-03-13 15:49:33 +0100

    Missing kubeconfig setting

--- service/service.go
+++ service/service.go
@@ -54,6 +54,7 @@ func New(config Config) (*Service, error) {
 
 			Address:   config.Viper.GetString(config.Flag.Service.Kubernetes.Address),
 			InCluster: config.Viper.GetBool(config.Flag.Service.Kubernetes.InCluster),
+			KubeConfig: config.Viper.GetBool(config.Flag.Service.Kubernetes.KubeConfig),
 			TLS: k8srestconfig.ConfigTLS{
 				CAFile:  config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.CAFile),
 				CrtFile: config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.CrtFile),
