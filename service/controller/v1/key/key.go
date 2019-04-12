bf76010ed1edc1ce1de385502e603db9ccd26b04
Tim Schindler <xh3b4sd@gmail.com>
2019-04-12 16:18:06 +0200

    drop node resource (#86)

--- service/controller/v1/key/key.go
+++ service/controller/v1/key/key.go
@@ -5,38 +5,16 @@ import (
 	"github.com/giantswarm/microerror"
 )
 
-func ClusterAPIEndpoint(customObject v1alpha1.NodeConfig) string {
-	return customObject.Spec.Guest.Cluster.API.Endpoint
-}
-
 func ClusterEndpointFromDrainerConfig(drainerConfig v1alpha1.DrainerConfig) string {
 	return drainerConfig.Spec.Guest.Cluster.API.Endpoint
-}
-
-func ClusterID(customObject v1alpha1.NodeConfig) string {
-	return customObject.Spec.Guest.Cluster.ID
 }
 
 func ClusterIDFromDrainerConfig(drainerConfig v1alpha1.DrainerConfig) string {
 	return drainerConfig.Spec.Guest.Cluster.ID
 }
 
-func NodeName(customObject v1alpha1.NodeConfig) string {
-	return customObject.Spec.Guest.Node.Name
-}
-
 func NodeNameFromDrainerConfig(drainerConfig v1alpha1.DrainerConfig) string {
 	return drainerConfig.Spec.Guest.Node.Name
-}
-
-func ToCustomObject(v interface{}) (v1alpha1.NodeConfig, error) {
-	p, ok := v.(*v1alpha1.NodeConfig)
-	if !ok {
-		return v1alpha1.NodeConfig{}, microerror.Maskf(wrongTypeError, "expected '%T', got '%T'", &v1alpha1.NodeConfig{}, v)
-	}
-	o := *p
-
-	return o, nil
 }
 
 func ToDrainerConfig(v interface{}) (v1alpha1.DrainerConfig, error) {
@@ -47,10 +25,6 @@ func ToDrainerConfig(v interface{}) (v1alpha1.DrainerConfig, error) {
 	o := *p
 
 	return o, nil
-}
-
-func VersionBundleVersion(customObject v1alpha1.NodeConfig) string {
-	return customObject.Spec.VersionBundle.Version
 }
 
 func VersionBundleVersionFromDrainerConfig(drainerConfig v1alpha1.DrainerConfig) string {
