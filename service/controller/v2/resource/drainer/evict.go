0b948ee1bdef7d99e0ba191ee968d7ab4b342c17
Tim Schindler <xh3b4sd@gmail.com>
2020-04-27 11:38:22 +0200

    fix linter issues (#126)

--- service/controller/v2/resource/drainer/evict.go
+++ service/controller/v2/resource/drainer/evict.go
@@ -3,7 +3,7 @@ package drainer
 import (
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/to"
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/api/policy/v1beta1"
 	apismetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
