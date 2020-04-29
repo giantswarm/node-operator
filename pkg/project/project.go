e1ddb1e044c2ed28d49528cd412675d66cc82492
Volodymyr Stoiko <me@volodymyrstoiko.com>
2020-04-29 15:08:23 +0300

    Release v1.0.1 (#131)

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "node-operator"
 	source      = "https://github.com/giantswarm/node-operator"
-	version     = "1.0.0"
+	version     = "1.0.1"
 )
 
 func Description() string {
