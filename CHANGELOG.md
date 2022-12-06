86c75141eb6457cec0099227148f1aede2c623c1
Nick Jüttner <nick@giantswarm.io>
2022-12-06 11:48:37 +0100

    ignore cert-exporter-deployment

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+
+- Ignore `cert-exporter-deployment.`
+
 ## [1.4.1] - 2022-10-10
 
 ### Fixed
--- service/controller/resource/drainer/create.go
+++ service/controller/resource/drainer/create.go
@@ -167,6 +167,10 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 				// we are aligning here with community as 'kubectl drain' also ignore them
 				continue
 			}
+			if key.IsDeploymentPod(p.Name) {
+				// ignore special deployments (cert-exporter-deployment)
+				continue
+			}
 			if key.IsEvictedPod(p) {
 				// we don't need to care about already evicted pods
 				continue
